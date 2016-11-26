d94ba31d9b862fbca6f673c04ddf05dfe76ed406
Dovydas Gulbinas <dovydasgulbinas@gmail.com>
2016-11-27 00:44:57 +0200

    working power commit

--- main/main.ino
+++ main/main.ino
@@ -4,13 +4,19 @@
 #define SLAVE_ADDRESS 0x04
 int number = 0;
 int state = 0;
-float power = 12345.67;
+float power = 0;
 
 int relay_pin = 12;
 
 #define GET_BYTES 1
 #define ON 2
 #define OFF 3
+
+const float refVoltage = 4.44;
+const float currentConstant1 = 0.1;// V:A
+const float currentConstant2 = 0.066;// V:A
+const float currentVoltageConstant1 = refVoltage/(1024*currentConstant1);
+const float currentVoltageConstant2 = refVoltage/(1024*currentConstant2);
 
 
 void setup() {
@@ -24,13 +30,32 @@ Wire.begin(SLAVE_ADDRESS);
 
 // define callbacks for i2c communication
 Wire.onReceive(receiveData);
-//Wire.onRequest(sendData);
+Wire.onRequest(sendData);
 
 Serial.println("Ready!");
 }
 
 void loop() {
-delay(100);
+  int sensorValue = analogRead(A0);
+    int current_reading = analogRead(A1);
+
+  	float ac_voltage = sensorValue * (10.0/1023.0);
+   	ac_voltage = (ac_voltage - 5.0);
+  	ac_voltage = ac_voltage * 110.190;
+  	float current = current_reading*currentVoltageConstant2 -1.71;
+    power = abs(current * ac_voltage);
+
+  	Serial.print(" Voltage: ");
+    Serial.print(ac_voltage);
+  	Serial.print(" Current: ");
+  	Serial.print(current);
+  	Serial.print(" Power: ");
+  	Serial.print(power);
+  	Serial.print(" CS-val: ");
+  	Serial.println(sensorValue);
+
+
+delay(1000);
 }
 
 // callback for received data/
@@ -49,9 +74,6 @@ else if (number == OFF){
 digitalWrite(relay_pin, LOW);
 Serial.println("TURNING OFF-RELAY ON CYKA");
 }
-else if (number == GET_BYTES){
-sendData();
-}
 }
 }
 
@@ -60,25 +82,27 @@ void sendData(){
 
 //int pint = (int)power;
 long pint = static_cast<long>(power);
-
-//if(number == GET_BYTES){
+if(number == GET_BYTES){
 byte byte1 = pint >> 24; // gets msb
 byte byte2 = pint >> 16; // gets msb
 byte byte3 = pint >> 8; // gets msb
 byte byte4 = pint; // gets lsb
-
+Serial.println("---------------MAKING--------------");
 Serial.println(power);
 Serial.println(pint);
 Serial.println("Bytes below");
-// Serial.println(byte1);
-// Serial.println(byte2);
-// Serial.println(byte3);
-// Serial.println(byte4);
+Serial.println(byte1);
+Serial.println(byte2);
+Serial.println(byte3);
+Serial.println(byte4);
 Wire.write(byte1);
 Wire.write(byte2);
 Wire.write(byte3);
 Wire.write(byte4);
-//}
+
+Serial.println("---------------SENT--------------");
+
+}
 
 
 }
